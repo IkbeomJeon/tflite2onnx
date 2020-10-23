82f82d32af32ed7d20d9fcb376c5cf9d3cc31396
IkbeomJeon <jeonikbeom@gmail.com>
2020-10-23 14:11:45 +0900

    add pycharm project settings.

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
-import tflite2onnx
+import tflite2onnx as tf
 
 #filename = 'log.float32'
 filename = 'object_detection_3d_sneakers'
 
-
 tflite_path = f'assets/tests/{filename}.tflite'
 onnx_path = f'./{filename}.onnx'
 
-
-tflite2onnx.convert(tflite_path, onnx_path)
+tf.convert(tflite_path, onnx_path)
